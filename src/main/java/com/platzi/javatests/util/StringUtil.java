6dfd754ccc1d2ddca6caf8c70314e8190fe3cbd8
cesardramirez <cesardavid89@gmail.com>
2019-04-22 20:01:39 -0500

    feat(main-test): Simulación de como funcionaría un test de una función

---
+++ src/main/java/com/platzi/javatests/util/StringUtil.java
@@ -0,0 +1,14 @@
+package com.platzi.javatests.util;
+
+public class StringUtil {
+
+    public static String repeat(String str, int times) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < times; i++) {
+            result.append(str);
+        }
+
+        return result.toString();
+    }
+}
