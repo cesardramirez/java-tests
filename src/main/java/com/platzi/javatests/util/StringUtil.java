b6dfd73a779919fae1c57596ef01c5353ff90f96
cesardramirez <cesardavid89@gmail.com>
2019-05-19 18:59:13 -0500

    feat(separate-func): Convención de pruebas y expected Exception.

--- src/main/java/com/platzi/javatests/util/StringUtil.java
+++ src/main/java/com/platzi/javatests/util/StringUtil.java
@@ -5,6 +5,10 @@ public class StringUtil {
     public static String repeat(String str, int times) {
         StringBuilder result = new StringBuilder();
 
+        if (times < 0) {
+            throw new IllegalArgumentException("negative times not allowed");
+        }
+
         // Se debe eliminar el = para que los test funcionen.
         for (int i = 0; i < times; i++) {
             result.append(str);
--- src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -7,8 +7,22 @@ import static org.junit.Assert.assertEquals;
 public class StringUtilTest {
 
     @Test
-    public void testRepeat() {
-        assertEquals("holaholahola", StringUtil.repeat("hola", 3));
+    public void testRepeatStringOnce() {
         assertEquals("hola", StringUtil.repeat("hola", 1));
+    }
+
+    @Test
+    public void testRepeatStringMultipleTimes() {
+        assertEquals("holaholahola", StringUtil.repeat("hola", 3));
+    }
+
+    @Test
+    public void testRepeatStringZeroTimes() {
+        assertEquals("", StringUtil.repeat("hola", 0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testRepeatStringNegativeTimes() {
+        StringUtil.repeat("hola", -1);
     }
 }
