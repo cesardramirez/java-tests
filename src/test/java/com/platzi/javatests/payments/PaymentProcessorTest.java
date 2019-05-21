23766c8d695fa06709ed7e2d679e3758f1d3f81f
cesardramirez <cesardavid89@gmail.com>
2019-05-20 21:46:31 -0500

    feat(before): Preparar el escenario para cada uno de los test.

--- src/test/java/com/platzi/javatests/payments/PaymentProcessorTest.java
+++ src/test/java/com/platzi/javatests/payments/PaymentProcessorTest.java
@@ -1,5 +1,6 @@
 package com.platzi.javatests.payments;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -22,19 +23,22 @@ public class PaymentProcessorTest {
     @Mock
     PaymentGateway paymentGateway;
 
+    @Before
+    public void setUp() {
+        paymentProcessor = new PaymentProcessor(paymentGateway);
+    }
+
     @Test
     public void makePayment_whenPaymentIsCorrect() {
-        paymentProcessor = new PaymentProcessor(paymentGateway);
-        // any(): Simulate with mockito classes that can not be tested directly (PaymentRequest).
         when(paymentGateway.requestPayment(any())).thenReturn(new PaymentResponse(OK));
+
         assertTrue(paymentProcessor.makePayment(1000));
     }
 
     @Test
     public void makePayment_whenPaymentIsWrong() {
-        paymentProcessor = new PaymentProcessor(paymentGateway);
-        // any(): Simulate with mockito classes that can not be tested directly (PaymentRequest).
         when(paymentGateway.requestPayment(any())).thenReturn(new PaymentResponse(ERROR));
+
         assertFalse(paymentProcessor.makePayment(1000));
     }
 }
